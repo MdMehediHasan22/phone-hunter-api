cb843560bd1b22c20095843571e9d99695c3f7c6
MdMehediHasan22 <mdmehedi3799@gmail.com>
2024-11-14 11:57:10 +0600

    completed the modal part

--- js/phone.js
+++ js/phone.js
@@ -1,12 +1,12 @@
-const loadPhone = async(searchText , isShowAll) =>{
+const loadPhone = async (searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
-    displayPhones(phones , isShowAll);
+    displayPhones(phones, isShowAll);
 }
 
-const displayPhones = (phones , isShowAll) =>{
-     
+const displayPhones = (phones, isShowAll) => {
+
     const phoneContainer = document.getElementById('phones-container');
     //clear container before search
 
@@ -14,23 +14,23 @@ const displayPhones = (phones , isShowAll) =>{
 
     const showAllContainer = document.getElementById('show-all-container');
 
-    if(phones.length>12 && !isShowAll){
+    if (phones.length > 12 && !isShowAll) {
         showAllContainer.classList.remove('hidden');
     }
-    else{
+    else {
         showAllContainer.classList.add('hidden');
     }
-    console.log('isSHowAll',isShowAll);
-   //display only first 12 phones if not show All
-    if(!isShowAll){
-        phones=phones.slice(0,12);
+    // console.log('isSHowAll', isShowAll);
+    //display only first 12 phones if not show All
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
     }
 
     phones.forEach(phone => {
-        console.log(phone);
+        // console.log(phone);
         const phoneCard = document.createElement('div');
-        phoneCard.classList='card bg-gray-100 p-4 shadow-xl';
-        phoneCard.innerHTML=`
+        phoneCard.classList = 'card bg-gray-100 p-4 shadow-xl';
+        phoneCard.innerHTML = `
         <figure>
                <img
                    src="${phone.image}" />
@@ -38,8 +38,8 @@ const displayPhones = (phones , isShowAll) =>{
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews ${phone.phone_name} whose ${phone.phone_name} does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+                <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">SHOW DETAILS</button>
             </div>
         </div>
         `;
@@ -50,20 +50,56 @@ const displayPhones = (phones , isShowAll) =>{
     toggleLoadingSpinner(false);
 }
 
-const handleSearch = (isShowAll) =>{
+const handleShowDetail = async (id) => {
+    // console.log('clicked show Details', id);
+    //load single phone Data\
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phoneDetails =data.data; 
+    showPhoneDetails(phoneDetails);
+
+}
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+
+    const phoneName = document.getElementById('phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('phone-details-container');
+    showDetailContainer.innerHTML = `
+    
+    <img class="mx-auto" src="${phone.image}" alt=""/>
+    <p><span class="font-bold text-lg">Storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span class="font-bold text-lg">Display Size:</span>${phone?.mainFeatures?.displaySize}</p>
+    <p><span class="font-bold text-lg">Chipset:</span>${phone?.mainFeatures?.chipSet}</p>
+    <p><span class="font-bold text-lg">Memory:</span>${phone?.mainFeatures?.memory}</p>
+    <p><span class="font-bold text-lg">Slug:</span>${phone.slug}</p>
+    <p><span class="font-bold text-lg">Release Data:</span>${phone.releaseDate}</p>
+    <p><span class="font-bold text-lg">Brand:</span>${phone.brand}</p>
+    <p><span class="font-bold text-lg">GPS:</span>${phone?.others?.GPS}</p>
+  
+
+    `;
+
+    //show the modal
+    show_details_modal.showModal();
+}
+
+const handleSearch = (isShowAll) => {
     toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     console.log(searchText);
-    loadPhone(searchText ,isShowAll);
+    loadPhone(searchText, isShowAll);
 }
 
-const toggleLoadingSpinner = (isLoading) =>{
+const toggleLoadingSpinner = (isLoading) => {
     const loadingSpinner = document.getElementById('loading-spinner');
-    if(isLoading){
+    if (isLoading) {
         loadingSpinner.classList.remove('hidden');
     }
-    else{
+    else {
         loadingSpinner.classList.add('hidden');
     }
 
